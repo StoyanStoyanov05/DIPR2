633c9d848d757e3bf5930e0802d369c4b8b31c11
StoyanStoyanov05 <StoyanStoyanov05@mail.bg>
2025-01-21 10:05:55 +0200

    New models

---
+++ app/Models/ShoppingListItem.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ShoppingListItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'shopping_list_id',  
+        'ingredient_id',     
+        'quantity',          
+        'unit',              
+        'is_purchased',      
+    ];
+
+    public function shoppingList()
+    {
+        return $this->belongsTo(ShoppingList::class);
+    }
+
+    public function ingredient()
+    {
+        return $this->belongsTo(Ingredient::class);
+    }
+}
