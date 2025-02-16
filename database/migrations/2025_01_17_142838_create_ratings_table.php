19737bc386378dab514cd72714a74ae227adda1d
StoyanStoyanov05 <StoyanStoyanov05@mail.bg>
2025-02-16 22:00:46 +0200

    Initial commit

--- database/migrations/2025_01_17_142838_create_ratings_table.php
+++ database/migrations/2025_01_17_142838_create_ratings_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('recipe_id')->constrained()->onDelete('cascade');
-            $table->integer('rating')->unsigned();
+            $table->tinyInteger('rating')->unsigned();
             $table->timestamps();
         });
     }
